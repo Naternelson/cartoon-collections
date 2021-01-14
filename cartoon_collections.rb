ea8f28cb4fee6a337169c75dc04cf5274dac91eb
Nate <7nater.nelson@gmail.com>
2021-01-14 15:27:49 -0700

    commit tree 02a3e7dafc6eb482706c86e6088ad421dfa73787

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)# code an argument here
+  arr.each_with_index{|name, i| puts"#{i+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(heros)# code an argument here
+  heros.collect {|hero|"#{hero.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)# code an argument here
+  calls.any?{|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)# code an argument here
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_size = cheese_types.length
+  i = 0
+  while i < cheese_size
+    return arr.find {|cheese| cheese == cheese_types[i]}  if arr.include?(cheese_types[i])
+    i += 1
+  end
 end
